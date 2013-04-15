839a06ae5b4cbc2218f0e1a823c9ecbadf05cd97
Mike Griffin <mgriffin@museum.ie>
2013-04-15 14:41:29 +0100

    Removes the test database creation

--- recipes/database.rb
+++ recipes/database.rb
@@ -9,25 +9,3 @@
 
 include_recipe "mysql::server"
 include_recipe "database::mysql"
-
-mysql_database_user "lvac" do
-  connection({
-    :host => "localhost",
-    :username => "root",
-    :password => node["mysql"]["server_root_password"]
-  })
-  password "Runn3r"
-  database_name "lvac_test"
-  host "localhost"
-  privileges [:all]
-  action :grant
-end
-
-mysql_database "lvac_test" do
-  connection({
-    :host => "localhost",
-    :username => "lvac",
-    :password => "Runn3r"
-  })
-  action :create
-end
